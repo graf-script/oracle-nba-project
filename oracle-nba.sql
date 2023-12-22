521ef8076a05b0c46914ee7d1a45cb659c1ead61
Kirill Makhnovskyi <52702982+graf-script@users.noreply.github.com>
2023-12-22 12:32:52 +0200

    Create oracle-nba.sql
    nba project start

---
+++ oracle-nba.sql
@@ -0,0 +1,91 @@
+-- START SESSION
+-- creating some tables and inserting data
+
+CREATE TABLE players 
+(
+    player_num NUMBER(3) NOT NULL,
+    full_name VARCHAR2(50) NOT NULL UNIQUE,
+    position_id NUMBER(2) NOT NULL,
+    team_id NUMBER(2) NOT NULL,
+    fav_food VARCHAR(50) NOT NULL,
+    dob DATE NOT NULL
+);
+
+CREATE TABLE positions 
+(
+    position_id NUMBER(2) NOT NULL UNIQUE,
+    position_name VARCHAR2(50) NOT NULL UNIQUE
+);
+
+INSERT INTO positions
+    (position_id, position_name)
+        VALUES(1, 'Heavy Forward');        
+INSERT INTO positions
+    (position_id, position_name)
+        VALUES(2, 'Playing Guard');
+INSERT INTO positions
+    (position_id, position_name)
+        VALUES(3, 'Center');
+
+CREATE TABLE teams 
+(
+    team_id NUMBER(2) NOT NULL UNIQUE,
+    team_name VARCHAR2(50) NOT NULL UNIQUE
+);
+
+INSERT INTO teams
+    (team_id, team_name)
+        VALUES(1, 'Los Angeles Lakers');
+INSERT INTO teams
+    (team_id, team_name)
+        VALUES(2, 'Golden State Warriors');
+INSERT INTO teams
+    (team_id, team_name)
+        VALUES(3, 'San Antonio Spurs');
+        
+INSERT INTO players 
+    (player_num, full_name, position_id, team_id, fav_food, dob)
+        VALUES(23, 'Lebron James', 1, 1, 'Taco', '30/12/1984');       
+INSERT INTO players 
+    (player_num, full_name, position_id, team_id, fav_food, dob)
+        VALUES(30, 'Steph Curry', 2, 2, 'Pizza', '14/03/1988');
+INSERT INTO players 
+    (player_num, full_name, position_id, team_id, fav_food, dob)
+        VALUES(1, 'Victor Wembanyama', 3, 3, 'Sushi Rolls', '04/01/2004');
+   
+SELECT 
+p.player_num,
+p.full_name,
+p.position_id,
+s.position_name,
+p.team_id,
+t.team_name,
+p.fav_food,
+p.dob
+FROM players p INNER JOIN positions s
+ON p.position_id = s.position_id
+JOIN teams t
+ON p.team_id = t.team_id;
+
+commit;
+
+CREATE OR REPLACE VIEW all_players_info AS
+SELECT
+p.player_num,
+p.full_name,
+p.position_id,
+s.position_name,
+p.team_id,
+t.team_name,
+p.fav_food,
+p.dob
+FROM players p INNER JOIN positions s
+ON p.position_id = s.position_id
+JOIN teams t
+ON p.team_id = t.team_id;
+
+SELECT * FROM all_players_info;
+
+-- NEXT SESSION        
+-- Adding some more Constraints
+-- Using select statement and single row function
